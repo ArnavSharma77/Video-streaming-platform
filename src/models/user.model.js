d63c3b651d6efefef7b6d236fea7c4c2e7735abd
ArnavSharma77 <115538878+ArnavSharma77@users.noreply.github.com>
2024-07-28 04:02:26 +0530

    "add register controller"

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new Schema(
 //prehook , jab bhi data ja/save ho rha ho usse pehle kuchh krna ho
 userSchema.pre ("save" , async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password , 10)
+    this.password = await bcrypt.hash(this.password , 10)
     next()
 })   
 
