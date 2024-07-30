24f9abf1661a2bf539a30d05f29ba6744ad7d8b2
ArnavSharma77 <115538878+ArnavSharma77@users.noreply.github.com>
2024-07-30 08:51:47 +0530

    add controller , routes etc

--- src/app.js
+++ src/app.js
@@ -19,11 +19,26 @@ app.use(cookieParser())
 
 //routes import
 import userRouter from "./routes/user.routes.js"
-
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
 //router ko laane ke liye middleware laana pdega (this is the only syntax)
 app.use("/api/v1/users",userRouter) // /users is prefix  /api/version btana shi rehta hai
 //http://localhost:8000/api/v1/users/register -->user dekhte hi userRouter pe jaayega
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 export {app}
