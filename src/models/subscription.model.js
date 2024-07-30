ced9f616eac81698b35053953a5b7840bb9ba32d
ArnavSharma77 <115538878+ArnavSharma77@users.noreply.github.com>
2024-07-30 07:02:50 +0530

    add watchHistory endpoint

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,8 @@
 import mongoose , {Schema} from "mongoose";
 
 const subscriptionSchema = new Schema({
+    //technically both are channel and user
+    //not an array (how would it work)
     subscriber : {
         type : Schema.Types.ObjectId, // one who is subscribing
         ref : "User"
