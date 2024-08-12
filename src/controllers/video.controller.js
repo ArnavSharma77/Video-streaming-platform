704f9da12b77162fb9dfdebc70d26623bafa2731
ArnavSharma77 <115538878+ArnavSharma77@users.noreply.github.com>
2024-08-12 14:30:02 +0530

    add video controller functionality

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -107,8 +107,6 @@ const getAllVideos = asyncHandler(async (req, res) => {
     }
   });
 
-
-
 const publishAVideo = asyncHandler(async (req, res) => {
     // get video, upload to cloudinary, create video
     //req.user - user , check if there or not 
@@ -273,14 +271,53 @@ const getVideoById = asyncHandler(async (req, res) => {
  
  })
 
-const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: delete video
+ const deleteVideo = asyncHandler(async (req, res) => {
+  // delete video
+ try {
+   const { videoId } = req.params
+   
+   if(!videoId) throw new ApiError(400,"videoId missing");
+   
+   if(!req.user) throw new ApiError(400,"user not loggedIn");
+
+   const userId = req.user._id;
+   const video = await Video.findById(videoId);
+   if(!video) throw new ApiError(400,"video with this videoId is missing")
+   const ownerId = video?.owner;
+   // console.log(new String(userId));
+   // console.log(JSON.stringify(ownerId));
+
+   if(JSON.stringify(ownerId) !== JSON.stringify(userId)) throw new ApiError(400,"login with owner id")
+
+   const deleted = await Video.findByIdAndDelete(new mongoose.Types.ObjectId(videoId));
+   if(video.thumbnailPublicId){
+
+      deleteFromCloudinary(video.thumbnailPublicId).catch(err=>console.log(err));
+   }
+   if(video.videoFilePublicId){
+    
+      deleteFromCloudinary(video.videoFilePublicId,"video").catch(err=>console.log(err));
+   }
+  // console.log(deleted)
+
+   return res
+   .status(200)
+   .json(
+       new ApiResponse(200,{info:`video : ${video.title} is deleted`},"video deleted successFully")
+   )
+ } catch (error) {
+  res
+  .status(error?.statusCode||500)
+  .json({
+     status:error?.statusCode||500,
+     message:error?.message||"some error in deleting a video"
+  })
+ }
 })
 
-const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+// const togglePublishStatus = asyncHandler(async (req, res) => {
+//     const { videoId } = req.params
+// })
 
 export {
     getAllVideos,
@@ -288,5 +325,5 @@ export {
     getVideoById,
     updateVideo,
     deleteVideo,
-    togglePublishStatus
+    // togglePublishStatus
 }
